39e03df4cf49db9d0a97e7da16140371dd33ed77
Amazingh0rse <therahbek@gmail.com>
2020-10-30 15:19:05 +0100

    added liftingstate

--- thursday/thursday/src/App.js
+++ thursday/thursday/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NameForm from './FormDemo';
+import StateDemo from './liftingUp';
 
 function App() {
   return (
     <div className="App">
-      
+      <StateDemo />
     </div>
   );
 }
